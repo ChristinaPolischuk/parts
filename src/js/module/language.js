73d3db2f68d0fd4ac019fb17bdc8c4c0f664fb6f
frontEndDevel <front_end_dev@yahoo.com>
2022-05-21 00:30:43 +0200

    Added sections posts, categories and footer

--- src/js/module/language.js
+++ src/js/module/language.js
@@ -1,6 +1,13 @@
 module.exports = () => {
-    $(".language").on("click", function () {
+    $(".js-select-language").on("click", function () {
         $(this).toggleClass("is-active");
+    });
+
+    $(".js-select-language-option").on("click", function () {
+        let select_val = $(this).attr("data-select-val");
+        let select_lang = $(this).closest(".js-select-language");
+        $(select_lang).children(".js-language-selected").html($(this).html());
+        $(select_lang).children("input").val(select_val);
     });
 };
 
