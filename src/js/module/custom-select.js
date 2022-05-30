8a429a9933b3281e330d7fa72343eb001f2a677d
frontEndDevel <front_end_dev@yahoo.com>
2022-05-30 14:47:25 +0200

    added dropdown select in filter form

--- src/js/module/custom-select.js
+++ src/js/module/custom-select.js
@@ -2,5 +2,12 @@ module.exports = () => {
     $(".custom-select").on("click", function () {
         $(this).toggleClass("is-active");
     });
+
+    $(".custom-select__item").on("click", function () {
+        let select_val = $(this).attr("data-select-val");
+        let select = $(this).closest(".custom-select");
+        $(select).children(".custom-select__header").html($(this).html());
+        $(select).children("input").val(select_val);
+    });
 };
 
